59137b6e9c46930e07675cba422168d822dc67e8
Lumi <shaicloud@gmail.com>
2023-03-14 22:23:52 -0400

    final changes before modularizing project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("project started successfully");
+const homeButton = document.getElementById("home");
+const menuButton = document.getElementById("menu");
+const contactButton = document.getElementById("contact");
+
+homeButton.addEventListener("click", () => {
+	console.log("home button clicked");
+});
+
+menuButton.addEventListener("click", () => {
+	console.log("menu button clicked");
+});
+
+contactButton.addEventListener("click", () => {
+	console.log("contact button clicked");
+});
