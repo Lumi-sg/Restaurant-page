f80ed8392635816ab409e2e4be36cced907a60f9
Lumi <shaicloud@gmail.com>
2023-03-16 02:09:11 -0400

    finished initial page load

--- src/modules/initPage.js
+++ src/modules/initPage.js
@@ -5,10 +5,36 @@ const loadPage = () => {
 
 	createImage();
 
-	// const footer = document.createElement("p");
-	// footer.className = "footer";
-	// footer.innerHTML = "Built by Lumi&nbsp;&nbsp;&nbsp";
-	// document.body.append(footer);
+	createFooter();
+
+	function createFooter() {
+		const pElement = document.createElement("p");
+
+		const aElement1 = document.createElement("a");
+		aElement1.innerText = "Built by Lumi ";
+
+		const aElement2 = document.createElement("a");
+		aElement2.href = "https://github.com/Lumi-sg/Restaurant-page";
+		aElement2.style.textDecoration = "none";
+		aElement2.target = "_blank";
+		aElement2.rel = "noopener noreferrer";
+
+		const iconElement = document.createElement("i");
+		iconElement.className = "fa";
+		iconElement.style.fontSize = "24px";
+		iconElement.innerText = "\uf09b";
+
+		aElement2.appendChild(iconElement);
+
+		pElement.appendChild(aElement1);
+		pElement.appendChild(aElement2);
+
+		const divElement = document.createElement("div");
+		divElement.className = "footer";
+
+		divElement.appendChild(pElement);
+		document.body.appendChild(divElement);
+	}
 
 	function createHeader() {
 		const storeTitle = document.createElement("div");
