d93f108e644140156551a65692d0784794235554
Lumi <shaicloud@gmail.com>
2023-03-15 23:52:43 -0400

    initial page load module

--- src/modules/initPage.js
+++ src/modules/initPage.js
@@ -1,5 +1,12 @@
 const loadPage = () => {
 	console.log("init page called");
+	const contentSection = document.querySelector(".content");
+
+	const image = document.createElement("img");
+	image.src = "./b8018bbf-9fea-4f70-9c24-017e50480cd8.jpeg";
+	image.setAttribute("class", "shawarma");
+	image.setAttribute("alt", "picture of the foods offered");
+	contentSection.appendChild(image);
 };
 
 export { loadPage };
